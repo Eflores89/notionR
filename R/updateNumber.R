716a6cce1b6d495de8d7d21a3244438709f12c54
Eduardo Flores <eduardo@enelmargen.org>
2023-01-02 13:32:23 +0100

    adds support to update numbers - enhancement #2

---
+++ R/updateNumber.R
@@ -0,0 +1,29 @@
+#' Updates a Number Property
+#'
+#'  Id refers to a page in a database, and should be normalized using normalizeChromeId().
+#'  @param secret API token
+#'  @param id Page id to be updated
+#'  @param property_name name of property to update (should be a number type property)
+#'  @param value value to update
+#'
+#' @importFrom httr PATCH
+updateNumber <- function(secret, id, property_name, value){
+
+  payload  <- sprintf("{\"properties\":{\"%s\":{\"number\":%s}}}",
+                      property_name, value
+                      )
+
+  auth_secret <- paste0("Bearer ", secret)
+
+  headers = c(
+    `Authorization` = auth_secret,
+    `Notion-Version` = '2022-02-22',
+    `Content-Type` = 'application/json' )
+
+  res <- httr::PATCH(url = paste0('https://api.notion.com/v1/pages/', id),
+                     httr::add_headers(.headers = headers),
+                     body = payload,
+                     encode = "json")
+  d <- httr::content(res)
+  return(d)
+}
